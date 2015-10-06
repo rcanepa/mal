969effe191f15cb0fd2ab09acf4d0e97a6a1fb79
Renzo Canepa <rcanepag@gmail.com>
2015-10-06 18:06:41 -0300

    Changed the flow to incorporate an input and token stream. Now, the tokenizer (lexer) work without regular expressions.

--- my-js/reader.js
+++ my-js/reader.js
@@ -1,21 +1,209 @@
 'use strict';
 
 /**
- * Reader factory
- * @type {{length: number, index: number, add: Function, peek: Function, next: Function}}
+ * MAL expression (code) -> inputStream -> tokenStream -> Array
  */
-var readerFactory = {
-  length: 0,
-  index: 0,
-  add: function(element) {
-    Array.prototype.push.call(this, element);
-  },
-  peek: function() {
-    return this[this.index];
-  },
-  next: function() {
-    return this[this.index++];
+
+/**
+ * Consume a string with a MAL expression and return an inputStream
+ * object. This object allows us to iterate over the stream in a
+ * char by char basis.
+ * @param input
+ * @returns {{next: next, peek: peek, eof: eof, error: error}}
+ */
+var inputStream = function(input) {
+  var pos = 0;
+  var line = 1;
+  var col = 0;
+
+  return {
+    next: next,
+    peek: peek,
+    eof: eof,
+    error: error
+  };
+
+  /**
+   * Return a char (string of size 1) that is in the pos position
+   * of the stream and increment pos by one. It also updates the
+   * current col position and line position.
+   * @returns {String}
+   */
+  function next() {
+    var char = input[pos++];
+    if (char === "\n") {
+      line++;
+      col = 0;
+    }
+    else {
+      col++;
+    }
+    return char;
   }
+
+  /**
+   * Return a char (string of size 1) that is in the pos position
+   * of the stream.
+   * @returns {String}
+   */
+  function peek() {
+    return input[pos];
+  }
+
+  /**
+   * Returns true if the end of the stream was reached.
+   * @returns {boolean}
+   */
+  function eof() {
+    return input[pos] === undefined;
+  }
+
+  /**
+   * Consume a string message a thrown an Error with the message and
+   * the current stream position and column.
+   * @param msg
+   */
+  function error(msg) {
+    throw new Error(msg + ' (' + line + ':' + col + ') ');
+  }
+
+};
+
+/**
+ * TODO: support for strings, operators, symbols
+ * @param istream
+ * @returns {{peek: peek, next: next}}
+ */
+var tokenStream = function(istream) {
+  var current = null;
+
+  return {
+    peek: peek,
+    next: next
+  };
+
+  function read_next() {
+    var char;
+
+    read_until(is_white_space);
+
+    if (istream.eof()) {
+      return null;
+    }
+
+    char = istream.peek();
+
+    if (is_container(char)) {
+      return istream.next();
+    }
+
+    if (is_number(char)) {
+      return read_number();
+    }
+
+    istream.error('unrecognized char');
+
+  }
+
+  /**
+   * Consume a char and produce true if it is equal to a white space or a new line
+   * character.
+   * @param char
+   * @returns {boolean}
+   */
+  function is_white_space(char) {
+    return ' \n\r'.indexOf(char) >= 0;
+  }
+
+  /**
+   * Consume a char and produce true if the char is one of: (, ), [ or ].
+   * @param char
+   * @returns {boolean}
+   */
+  function is_container(char) {
+    return '()[]'.indexOf(char) >= 0;
+  }
+
+  /**
+   * Consume a char and produce true if it is a number or a '.'.
+   * @param char
+   * @returns {boolean}
+   */
+  function is_number(char) {
+    return (char !== '' && !is_white_space(char) && !isNaN(char)) || is_point(char);
+  }
+
+  /**
+   * Consume a char and produce true if it is equal to '.'.
+   * @param char
+   * @returns {boolean}
+   */
+  function is_point(char) {
+    return char === '.';
+  }
+
+  /**
+   * Iterate over the inputStream and return a string representing a number (integer or float).
+   * @returns {string}
+   */
+  function read_number() {
+    return read_until(is_number);
+  }
+
+  function read_until(predicate) {
+    var str = '';
+    while (!istream.eof() && predicate(istream.peek())) {
+      str += istream.next();
+    }
+    return str;
+  }
+
+  /**
+   * Return the current stream token.
+   * @returns {string}
+   */
+  function peek() {
+    return current || (current = read_next());
+  }
+
+  /**
+   * Return the current stream token and set the current to null. If current was already null,
+   * it will return call read_next() and return a new token.
+   * @returns {*}
+   */
+  function next() {
+    var token = current;
+    current = null;
+    return token || read_next();
+  }
+
+};
+
+/**
+ * Reader Factory
+ * @returns {{add: add, peek: peek, next: next}}
+ */
+var readerFactory = function() {
+  var length = 0;
+  var index = 0;
+  return {
+    add: add,
+    peek: peek,
+    next: next
+  };
+
+  function add(element) {
+    Array.prototype.push.call(this, element);
+  }
+
+  function peek() {
+    return this[index];
+  }
+
+  function next() {
+    return this[index++];
+  }
+
 };
 
 /**
@@ -26,11 +214,12 @@ var readerFactory = {
 function read_str(sexp) {
   var ast, reader;
   var tokens = tokenizer(sexp);
-  reader = Object.create(readerFactory);
+  reader = readerFactory();
   // console.log(tokens);
   tokens.forEach(function(element, index, array) {
     reader.add(element);
   });
+  // console.log(reader);
   ast = read_form(reader);
   return ast;
 }
@@ -39,11 +228,12 @@ function read_str(sexp) {
  * Consume a s-expression (string) and produce a flat list of tokens.
  * E.g.: '(+ (- 2 3) 4)' -> [ '(', '+', '(', '-', '2', '3', ')', '4', ')' ]
  * @param sexp  {String}
+ * TODO: implement this as a real lexer (without regular expressions)
  */
 function tokenizer(sexp) {
   sexp = remove_comments(sexp);
   sexp = clean_sexp(sexp);
-  // console.log(sexp);
+
   var re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/;
   var tokens = [];
   var match = sexp.match(re);
@@ -62,12 +252,15 @@ function tokenizer(sexp) {
  * an AST (nested lists).
  * E.g.: [ '(', '+', '(', '-', '2', '3', ')', '4', ')' ] -> [ '+', [ '-', '2', '3' ], '4' ]
  * @param reader  {Object}
- * @returns {Array}
+ * @returns {String, Number, Boolean, Array}
  */
 function read_form(reader) {
 
+  // console.log(reader);
+
   if (reader.length === 0) {
-    throw new Error('unexpected EOF while reading');
+    console.log('unexpected EOF while reading');
+    // throw new Error('unexpected EOF while reading');
   }
 
   var token = reader.peek();
@@ -77,7 +270,15 @@ function read_form(reader) {
   }
 
   if (token === ')') {
-    throw new Error('unexpected )');
+    throw new Error('expected \')\', got EOF');
+  }
+
+  if (token === '[') {
+    return read_vector(reader);
+  }
+
+  if (token === ']') {
+    throw new Error('expected \']\', got EOF');
   }
 
   if (token === '\'') {
@@ -112,14 +313,46 @@ function read_list(reader) {
   // drop the ( parenthesis
   reader.next();
 
-  while (reader.peek() !== ')') {
+  while (reader.peek() !== ')' && reader.peek() !== undefined) {
     lst.push(read_form(reader));
+  }
+
+  if (reader.peek() !== ')') {
+    // throw Error('expected \')\' got EOF');
+    console.log('expected \')\' got EOF');
   }
 
   // drop the ) parenthesis
   reader.next();
 
   return lst;
+}
+
+/**
+ * Consume a reader with its next token equal to '[', and return an Array with all
+ * the elements that are inside the parenthesis.
+ * @param reader {Object}
+ * @returns {Array}
+ */
+function read_vector(reader) {
+  var vec = [];
+
+  // drop the [ bracket
+  reader.next();
+
+  while (reader.peek() !== ']' && reader.peek() !== undefined) {
+    vec.push(read_form(reader));
+  }
+
+  if (reader.peek() !== ']') {
+    // throw Error('expected \']\' got EOF');
+    console.log('expected \']\' got EOF');
+  }
+
+  // drop the ] bracket
+  reader.next();
+
+  return vec;
 }
 
 /**
@@ -222,5 +455,7 @@ function clean_sexp(sexp) {
 }
 
 module.exports = {
-  read_str: read_str
+  read_str: read_str,
+  inputStream: inputStream,
+  tokenStream: tokenStream
 };
--- my-js/tests/reader.js
+++ my-js/tests/reader.js
@@ -1,5 +1,21 @@
+var test = require('tape');
 var reader = require('./../reader');
 
-var sexp = "'1";
-var ast = reader.read_str(sexp);
-console.log(ast);
+test('tokenStream basic assertions', function(assert) {
+  var sexp = '(10.1 2 33)';
+
+  var is = reader.inputStream(sexp);
+  var ts = reader.tokenStream(is);
+
+  assert.equal(ts.peek(), '(', 'first peek');
+  assert.equal(ts.peek(), '(', 'second peek (the result must be the same)');
+  assert.equal(ts.next(), '(', 'next item = (');
+  assert.equal(ts.next(), '10.1', 'next item = 10.1');
+  assert.equal(ts.next(), '2', 'next item = 2');
+  assert.equal(ts.peek(), '33', 'peek the same item = 33');
+  assert.equal(ts.next(), '33', 'next item = 33');
+  assert.equal(ts.next(), ')', 'next item = )');
+  assert.equal(ts.next(), null, 'next item = null');
+  assert.end();
+
+});
